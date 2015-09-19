af475674a0e696059d23b7943a2e4dd27ca99e10
Matthew Tiger <matthew.c.tiger@gmail.com>
2015-09-19 19:00:48 -0400

    Adding initial project version of MATH635.

---
+++ MATH635/homework/homework_3/homework_3_programs/matrix_power.m
@@ -0,0 +1,10 @@
+n = input('Please input an integer n used to calculate A^n: ');
+A = [2,4,6;0,2,3;0,0,2];
+
+disp('A^n: ');
+A_n = A^n;
+disp(A_n);
+
+disp('A^(-n): ');
+A_inverse_n = A^(-n);
+disp(A_inverse_n);
