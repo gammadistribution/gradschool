f2fbbc6e8ad6cc27d1c1a8281d7e776b7a050258
Matthew Tiger <matthew.c.tiger@gmail.com>
2015-09-27 20:52:22 -0400

    Made time_qr_factor a function

--- MATH635/homework/homework_4/time_qr_factor.m
+++ MATH635/homework/homework_4/time_qr_factor.m
@@ -1,16 +1,26 @@
-n = 50;
+function time_qr_factor(dimension, iterations)
+% Time function qr_factor for square tri-diagonal matrices. 
+%
+% For each integer dim from 1 to dimension, iterate from 1 to iterations,
+% creating a tridiagonal matrix of dimension dim and timing custom 
+% qr_factor vs qr for created matrix. Display number of times custom
+% factorization beat matlab's function for each matrix dimension.
 
-for i = 1:n
-    T = tridiag(i);
-    tic
-    [Q_1,R_1] = qr_factor(T);
-    time_1 = toc;
+for dim = 1:dimension
+    counter = 0;
+    for iteration = 1:iterations
+        T = tridiag(dim);
+        tic
+        [Q_1,R_1] = qr_factor(T);
+        time_1 = toc;
 
-    tic
-    [Q_2,R_2] = qr(T);
-    time_2 = toc;
+        tic
+        [Q_2,R_2] = qr(T);
+        time_2 = toc;
 
-    if time_1 < time_2
-        i, time_1, time_2
+        if time_1 < time_2
+            counter = counter + 1;
+        end
     end
+    fprintf('For dimension = %i, custom factorization faster in %i out of %i iterations.\n', dim, counter, iterations)
 end
