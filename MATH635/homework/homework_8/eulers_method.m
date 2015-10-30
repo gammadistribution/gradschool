ab385a42e5d96c9045189cdaf85f77160551c4f1
Matthew Tiger <matthew.c.tiger@gmail.com>
2015-10-30 19:52:25 -0400

    stubbed out eulers_method.m function

---
+++ MATH635/homework/homework_8/eulers_method.m
@@ -0,0 +1,12 @@
+function [t, u] = eulers_method(func, interval, eta, n, epsilon)
+% Computes the approximate solution to the first order cauchy problem 
+% over the passed interval where func(t, y) is the first order derivative 
+% of y. Note that eta is the initial condition of the problem, n is the
+% number of partitions of the interval to compute the solution, and epsilon
+% is the error in the value of the initial condition. 
+% 
+% Returns the points within the interval used to compute the solution, t, 
+% and the values of the first derivative at those points.
+
+end
+
