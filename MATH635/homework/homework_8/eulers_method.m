528e9699cb28d68fd1b5f43f4afe631aa90925f3
Matthew Tiger <matthew.c.tiger@gmail.com>
2015-10-30 22:09:13 -0400

    Finished problem 2 of homework_8 for MATH635

--- MATH635/homework/homework_8/eulers_method.m
+++ MATH635/homework/homework_8/eulers_method.m
@@ -7,6 +7,30 @@ function [t, u] = eulers_method(func, interval, eta, n, epsilon)
 % 
 % Returns the points within the interval used to compute the solution, t, 
 % and the values of the first derivative at those points.
+a = interval(1);
+b = interval(2);
 
+t = [];
+
+% Create array of points within interval.
+for i=1:n+1
+    if i == 1
+        element = a;
+    else
+        element = t(i-1) + 1/n;
+    end
+    t(i) = element;
 end
 
+% The first element of the solution array is the initial condition.
+u = [eta + epsilon];
+
+% Create the solution array.
+for i=1:length(t) - 1
+    u(i + 1) = u(i) + (t(i + 1) - t(i))*func(t(i), u(i));
+end  
+
+t = t';
+u = u';
+
+end
