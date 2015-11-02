3430dec83b7e9354848a4f73ba48726269d5e35a
Matthew Tiger <matthew@Legendre>
2015-11-02 13:56:06 -0500

    Added pictures to .tex document and changed graph to include original function.

--- MATH635/homework/homework_8/Interpolation/plot_lagrange_interpolation.m
+++ MATH635/homework/homework_8/Interpolation/plot_lagrange_interpolation.m
@@ -11,15 +11,22 @@ subplots = [];
 for i=1:length(subintervals) 
     subplots(i) = subplot(length(subintervals), 2, 1 + 2*(i-1));
 
+    hold on;
+    
     n = subintervals(i);
 
     nodes = uniform_nodes(interval, n);
     lagrange_interpolation = @(x) lagrange(x, nodes, @interpolating_function);
 
     Y = arrayfun(lagrange_interpolation, X);
+    Y_actual = arrayfun(@interpolating_function, X);
     
     p = plot(X, Y);
-    title(sprintf('Interpolating Polynomial with Uniform nodes for subintervals n = %i', n));
+    p_actual = plot(X, Y_actual);
+    
+    alpha(0.6);
+    
+    title(sprintf('Uniform nodes for subintervals n = %i', n));
 end
 
 
@@ -27,15 +34,26 @@ end
 for i=1:length(subintervals) 
     subplots(i) = subplot(length(subintervals), 2, 2*i);
 
+    hold on;
+    
     n = subintervals(i);
 
     nodes = chebyshev_nodes(interval, n);
     lagrange_interpolation = @(x) lagrange(x, nodes, @interpolating_function);
 
     Y = arrayfun(lagrange_interpolation, X);
+    Y_actual = arrayfun(@interpolating_function, X);
     
     p = plot(X, Y);
-    title(sprintf('Interpolating Polynomial with Chebyshev nodes for subintervals n = %i', n));
+    p_actual = plot(X, Y_actual);
+
+    alpha(0.6);
+
+    title(sprintf('Chebyshev nodes for subintervals n = %i', n));
 end
 
 linkaxes(subplots, 'xy');
+
+leg = legend([p, p_actual], {'P_n(f, x)', 'f(x)'}, 'Orientation', 'horizontal');
+
+set(leg, 'Position', [0.457407 0.0126621 0.118518 0.0585683], 'Units', 'normalized');
