ba4132c9e78bdc03db3d1a1f33bdef8e191e2508
Matthew Tiger <matthew@Legendre>
2015-11-15 18:25:26 -0500

    Modified threshold_table to output n values for all schemes

--- MATH635/MATLAB/DifferentialEquations/ODEs/threshold_table.m
+++ MATH635/MATLAB/DifferentialEquations/ODEs/threshold_table.m
@@ -8,9 +8,14 @@ interval = [0, 1];
 eta = 1/3;
 epsilon = 0.01;
 thresholds = [0.1, 0.01, 0.001];
+schemes = {@eulers_method, @heuns_method, @runge_kutta_method};
 
-subintervals = [];
-for threshold=1:length(thresholds)
-    n = convergence_threshold(@solution, @eulers_method, @first_derivative, interval, eta, epsilon, thresholds(threshold));  
-    subintervals = [subintervals, n];
+for scheme=1:length(schemes)
+    subintervals = [];
+    for threshold=1:length(thresholds)
+        n = convergence_threshold(@solution, schemes{scheme}, @first_derivative, interval, eta, epsilon, thresholds(threshold));  
+        subintervals = [subintervals, n];
+    end
+    scheme = func2str(schemes{scheme})
+    subintervals
 end
