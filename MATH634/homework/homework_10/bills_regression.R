7ecf254274dc78d18893b16314e58076e476c11b
Matthew Tiger <matthew@Legendre>
2015-11-18 22:39:23 -0500

    Finished homework_10 for MATH634

--- MATH634/homework/homework_10/bills_regression.R
+++ MATH634/homework/homework_10/bills_regression.R
@@ -16,8 +16,21 @@ bills.ts.Q3 <- quarter_variable(bills.ts, 3)
 bills.ts.Q4 <- quarter_variable(bills.ts, 4)
 
 bills.ts.regression_equation <- bills.ts ~ 0 + time(bills.ts) +
-    I(time(bills.ts)^2) + bills.ts.Q1 + bills.ts.Q2 + bills.ts.Q3 + bills.ts.Q4
+    bills.ts.Q1 + bills.ts.Q2 + bills.ts.Q3 + bills.ts.Q4
 bills.ts.regression <- lm(bills.ts.regression_equation)
 
-# The following tells us that all variables are significant using a significance level of alpha = 0.05.
+# The following tells us that all variables are significant using a
+# significance level of alpha = 0.05.
 summary(bills.ts.regression)
+
+bills.ts.residuals <- residuals(bills.ts.regression)
+
+bills.ts.regression
+
+pdf(file="residuals_acf.png")
+acf(bills.ts.residuals)
+dev.off()
+
+pdf(file="residuals_pacf.png")
+pacf(bills.ts.residuals)
+dev.off()
